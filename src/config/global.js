5b85c1f0975c744930e520a1a86358b2b7951cdb
Sebastian Trujillo <129899570+strujilloa@users.noreply.github.com>
2026-06-18 18:32:48 -0500

    Update empaquetados

--- src/config/global.js
+++ src/config/global.js
@@ -412,7 +412,7 @@ export default {
         {
           nombre: 'Claudia Johanna Gómez Pérez',
           cargo:
-            'Responsable Ecosistema de Recursos Educativos Digitales (RED)',
+            'Profesional G06. Responsable Ecosistema Virtual de Recursos Educativos Digitales',
           centro: 'Centro Agroturístico - Regional Santander',
         },
         {
